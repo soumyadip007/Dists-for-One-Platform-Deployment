a0091cac70572ff4d39af50a1191eccaa37425ab
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2021-07-29 13:21:09 +0530

    Feedback SPA Added

---
+++ Feedback SPA/dist/precache-manifest.140f5d48620920d28053ceca699f8d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39186464f60950dd740",
+    "url": "/feedback/css/app.2c790a55.css"
+  },
+  {
+    "revision": "6a8cfbaeccd76f95a1102b7caff69aa2",
+    "url": "/feedback/index.html"
+  },
+  {
+    "revision": "a39186464f60950dd740",
+    "url": "/feedback/js/app.0fd57c96.js"
+  },
+  {
+    "revision": "4215be7e4268bdc7456b",
+    "url": "/feedback/js/chunk-vendors.f97c0d8e.js"
+  },
+  {
+    "revision": "329f6ceab81bed3a5e2e7faab4a12840",
+    "url": "/feedback/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/feedback/robots.txt"
+  }
+]);
